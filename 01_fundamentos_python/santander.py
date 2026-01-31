a3b2b81d9f7aac034bcc28411a2859444861f300
Andoni Vázquez Arza <andoni.vazquez.arza@gmail.com>
2026-01-31 19:25:02 +0100

    Restore functions and add print for suma_variable
    Reintroduce functions and add print statement for suma_variable.

--- 01_fundamentos_python/santander.py
+++ 01_fundamentos_python/santander.py
@@ -6,7 +6,7 @@ def saludo(nombre,a,b):
 saludo("Adrián",4,7)
 
 #FUNCIONES LAMBDA (ANÓNIMAS)
-cuadrado = lambda x: x**2
+cuadrado = lambda x: x**2 #función diminuta y rápida // lambda argumentos : expresión
 print(cuadrado(5)) #imprime 25
 
 #FUNCIONES CON NÚMERO VARIABLE DE ARGUMENTOS
@@ -15,5 +15,6 @@ def suma_variable(*numeros): #asterisco para avisar de que cantidad variable
     for numero in numeros:
         total = total + numero
     return total #el valor que devuelve la función
+
 
 print(suma_variable(1,4,5,2)) #devuelve 12
